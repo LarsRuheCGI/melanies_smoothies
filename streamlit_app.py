7868117cacb2d2ba745973e7ad6bd03d9d3a4a46
LarsRuheCGI <lars.ruhe@CGI.com>
2024-06-19 12:50:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ import requests
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
